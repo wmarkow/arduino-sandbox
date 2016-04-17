eada8281a93194de7313002438dd121177af186a
Witold Markowski <witold.a.markowski@gmail.com>
2016-04-17 10:04:26 +0200

    Hello world on standard LCD Keypad Shield.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,11 +1,22 @@
 #include "sandbox.h"
 
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
 void setup() {
   Serial.begin(9600);
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print(F("Hello, world!"));
 }
 
 void loop() {
-	Serial.println(F("Hello world!"));
-
-	delay(1000);
+	// set the cursor to column 0, line 1
+	// (note: line 1 is the second row, since counting begins with 0):
+	lcd.setCursor(0, 1);
+	// print the number of seconds since reset:
+	lcd.print(millis() / 1000);
 }
