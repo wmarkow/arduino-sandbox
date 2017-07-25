5beb28ecf9eed1c7b1e952f3ce51b950281adac8
Witold Markowski <witold.a.markowski@gmail.com>
2017-07-25 21:47:37 +0100

    Cleanup.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,27 +1,3 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
@@ -36,7 +12,6 @@ void onDoorBellSwitchReleased();
 
 MonostableSwitch doorSwitch(2);
 
-// the setup function runs once when you press reset or power the board
 void setup() {
 	mySoftwareSerial.begin(9600);
 	  Serial.begin(9600);
@@ -56,26 +31,23 @@ void setup() {
 	  }
 	  Serial.println(F("DFPlayer Mini online."));
 
-	  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
-	  //myDFPlayer.playMp3Folder(1);  //Play the first mp3
+	  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
 }
 
-// the loop function runs over and over again forever
 void loop() {
 	doorSwitch.loop();
+}
 
-//	static unsigned long timer = millis();
+void onDoorBellSwitchPressed()
+{
+	Serial.println(F("Door bell switch pressed."));
+	myDFPlayer.playMp3Folder(11);
+}
 
-//	  if (millis() - timer > 5000) {
-//	    timer = millis();
-//	    myDFPlayer.next();  //Play next mp3 every 3 second.
-//	  }
-//
-//	  if (myDFPlayer.available()) {
-//	    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-//	  }                      // wait for a second
-
-
+void onDoorBellSwitchReleased()
+{
+	Serial.println(F("Door bell switch released."));
+	myDFPlayer.playMp3Folder(12);
 }
 
 void printDetail(uint8_t type, int value){
@@ -131,16 +103,4 @@ void printDetail(uint8_t type, int value){
     default:
       break;
   }
-}
-
-void onDoorBellSwitchPressed()
-{
-	Serial.println(F("Door bell switch pressed."));
-	myDFPlayer.playMp3Folder(11);
-}
-
-void onDoorBellSwitchReleased()
-{
-	Serial.println(F("Door bell switch released."));
-	myDFPlayer.playMp3Folder(12);
 }
