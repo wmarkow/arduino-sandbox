0d63429602d02342e3ed88518587049b2f426207
Witold Markowski <witold.a.markowski@gmail.com>
2017-07-25 21:24:40 +0100

    Add support for DFPlayer Mini.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -23,17 +23,125 @@
 */
 
 #include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+#include "MonostableSwitch.h"
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+void onDoorBellSwitchPressed();
+
+MonostableSwitch doorSwitch(2);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+	mySoftwareSerial.begin(9600);
+	  Serial.begin(9600);
+
+	  Serial.println();
+	  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+	  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+
+	  doorSwitch.setOnSwitchOn(&onDoorBellSwitchPressed);
+
+	  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+	    Serial.println(F("Unable to begin:"));
+	    Serial.println(F("1.Please recheck the connection!"));
+	    Serial.println(F("2.Please insert the SD card!"));
+	    while(true);
+	  }
+	  Serial.println(F("DFPlayer Mini online."));
+
+	  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+	  //myDFPlayer.playMp3Folder(1);  //Play the first mp3
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+	doorSwitch.loop();
+
+//	static unsigned long timer = millis();
+
+//	  if (millis() - timer > 5000) {
+//	    timer = millis();
+//	    myDFPlayer.next();  //Play next mp3 every 3 second.
+//	  }
+//
+//	  if (myDFPlayer.available()) {
+//	    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+//	  }                      // wait for a second
+
+
+}
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void onDoorBellSwitchPressed()
+{
+	Serial.println(F("Door bell switch pressed."));
+
+	long randoma = random(0, 2);
+	switch(randoma){
+	case 0:
+		myDFPlayer.playMp3Folder(1);
+		break;
+	case 1:
+		myDFPlayer.playMp3Folder(5);
+		break;
+	}
 }
