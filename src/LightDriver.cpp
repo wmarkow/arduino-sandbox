480b87516d365a205ba62ad42bcb889666793313
Witold Markowski <witold.a.markowski@gmail.com>
2017-09-02 20:10:29 +0100

    Introduce LED driver.

---
+++ src/LightDriver.cpp
@@ -0,0 +1,49 @@
+/*
+ * LightDriver.cpp
+ *
+ *  Created on: 30.08.2017
+ *      Author: wmarkow
+ */
+
+#include "LightDriver.h"
+#include <Arduino.h>
+
+LightDriver::LightDriver(uint8_t pin)
+{
+	this->pin = pin;
+
+	pinMode(pin, OUTPUT);
+	switchOff();
+}
+
+void LightDriver::switchOn()
+{
+	digitalWrite(pin, HIGH);
+}
+
+void LightDriver::switchOff()
+{
+	digitalWrite(pin, LOW);
+}
+
+bool LightDriver::isOn()
+{
+	if(digitalRead(pin) == HIGH)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+void LightDriver::toggle()
+{
+	if(isOn())
+	{
+		switchOff();
+	}
+	else
+	{
+		switchOn();
+	}
+}
