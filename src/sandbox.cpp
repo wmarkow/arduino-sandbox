d983f0f3a068c08fa3ef43348c1f534cf3a49482
Witold Markowski <witold.a.markowski@gmail.com>
2018-04-25 18:28:17 +0100

    Clean unused code.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,30 +1,7 @@
-/* dumpFreq.ino Example sketch for IRLib2
- * Illustrates how to simultaneously decode an IR stream and
- * measure modulation frequency using the IRfrequency class in
- * the IRLibFreq library. You must connect an IR learner such
- * as TSMP58000 to a hardware interrupt pin. Rather than use the
- * IRrecv receiver we recommend using the IRrecvPCI or IRrecvLoop
- * receiver because the 50 microsecond interrupts from IRrecv
- * can cause inaccurate results.
- */
-#include <IRLibAll.h>//Note this also includes "IRLibFreq.h"
-//#include <IRLibRecvPCI.h>
-//#include <IRLibFreq.h>
-//#include <IRLibDecodeBase.h>
-//#include <IRLibCombo.h>
-
+#include <Arduino.h>
 #include "TSMP58000.h"
 
-//NOTE: Previous versions of IRLib required you to pass the
-// interrupt number to be passed to the built-in "attachInterrupt()"
-// function however starting with IRLib 2.x you now pass the pin number.
 #define RECEIVER_PIN 2
-#define FREQUENCY_PIN 3
-
-//IRrecvPCI myReceiver(RECEIVER_PIN);
-//IRrecvLoop myReceiver(RECEIVER_PIN); //comment out previous line and un-comment this for loop version
-//IRfrequency myFreq(FREQUENCY_PIN);
-//IRdecode myDecoder;
 
 bool getResults(void);
 bool getResultsAsBits(void);
@@ -39,70 +16,12 @@ void setup()
    delay(2000);
    while (!Serial)
       ; //delay for Leonardo
-   if (F_CPU < 16000000)
-   {
-      Serial.println(
-            F("WARNING: CPU speed less than 16 MHz will be inaccurate"));
-      Serial.print(F("for frequencies over 45 kHz. Your CPU is:"));
-      Serial.print(F_CPU / 1000000, DEC);
-      Serial.println(F(" Mhz"));
-   }
-   int8_t intrNum = digitalPinToInterrupt(FREQUENCY_PIN);
-   if (intrNum == NOT_AN_INTERRUPT)
-   {
-      Serial.println(F("Invalid frequency pin number."));
-      while (1)
-      {
-      }; //infinite loop because of fatal error
-   }
-   Serial.print(F("Frequency interrupt="));
-   Serial.print(intrNum, DEC);
-   Serial.print(F(" Pin="));
-   Serial.println(FREQUENCY_PIN, DEC);
-   intrNum = digitalPinToInterrupt(RECEIVER_PIN);
-   if (intrNum == NOT_AN_INTERRUPT)
-   {
-      Serial.println(F("Invalid receiver pin number."));
-      while (1)
-      {
-      }; //infinite loop because of fatal error
-   }
-   Serial.print(F("Receiver interrupt="));
-   Serial.print(intrNum, DEC);
-   Serial.print(F(" Pin="));
-   Serial.println(RECEIVER_PIN, DEC);
-
-//   myReceiver.enableIRIn(); // Start the receiver
-//   myFreq.enableFreqDetect(); //starts interrupt routine to compute frequency
 }
 
 TSMP58000 tsmp;
 
 void loop()
 {
-   // decode protocol
-//  if (myReceiver.getResults()) {
-//     myReceiver.dump();
-//
-////    myDecoder.decode();
-//
-////    myDecoder.dumpResults();
-//    myReceiver.enableIRIn();
-//  }
-
-   // detect frequency
-//  if (myFreq.haveData()) {
-//      delay(500);  //it couldn't hurt to collect a little more just in case
-//      myFreq.disableFreqDetect();
-//      myFreq.dumpResults(true);//Change to "true" for more detail
-//      myFreq.enableFreqDetect();//Zero out previous results and restart ISR
-//    }
-
-   // moj kawalek kodu
-//   if (getResultsAsBits())
-//   {
-//      dump();
-//   }
    Serial.print(F("Waiting 3 sec for user click..."));
    if (tsmp.read())
    {
@@ -124,155 +43,8 @@ void loop()
 #define BUFFER_SIZE 512
 #define FRAME_TIMEOUT 7000
 uint16_t table[BUFFER_SIZE];
-volatile currentState_t currentState;
+volatile uint8_t currentState;
 uint16_t index = 0;
-
-#define MY_STATE_IDLE 0
-#define MY_STATE_WAITING_FOR_USER_CLICK 1
-#define MY_STATE_CHECKING_STARTUP 2
-#define MY_STATE_COLLECT_DATA 3
-#define MY_STATE_JUST_COUNT 4
-#define MY_STATE_FINISHED 10
-
-bool getResults(void)
-{
-   uint8_t oldState = HIGH;
-   uint8_t newState;
-   uint32_t startTime;
-   uint32_t veryStartTime;
-   uint32_t endTime;
-   volatile uint32_t currentMicros;
-   uint32_t firstChangeTimeStamp;
-
-   startTime = micros();
-   veryStartTime = startTime;
-   currentState = MY_STATE_IDLE;
-   index = 0;
-
-   while (currentState != MY_STATE_FINISHED)
-   {
-      currentMicros = micros();
-      switch (currentState)
-      {
-         case MY_STATE_IDLE:
-         {
-            Serial.println(F("Waiting 3 sec for user click"));
-            currentState = MY_STATE_WAITING_FOR_USER_CLICK;
-            break;
-         }
-         case MY_STATE_WAITING_FOR_USER_CLICK:
-         {
-            while (oldState == (newState = digitalRead(RECEIVER_PIN)))
-            {
-               currentMicros = micros();
-               uint32_t diff = currentMicros - veryStartTime;
-               if (diff > 3000000)
-               {
-                  currentState = MY_STATE_FINISHED;
-                  return true;
-               }
-            }
-            currentMicros = micros();
-            firstChangeTimeStamp = currentMicros;
-            table[index] = currentMicros - startTime;
-            startTime = firstChangeTimeStamp;
-            oldState = newState;
-            index++;
-//            currentState = MY_STATE_CHECKING_STARTUP;
-            currentState = MY_STATE_COLLECT_DATA;
-            break;
-         }
-         case MY_STATE_CHECKING_STARTUP:
-         {
-            while (oldState == (newState = digitalRead(RECEIVER_PIN)))
-            {
-               currentMicros = micros();
-               if (oldState == HIGH)
-               {
-                  if ((currentMicros - startTime) > 3000000)
-                  {
-                     currentState = MY_STATE_FINISHED;
-                     break;
-                  }
-               }
-            }
-            // the pin has changed
-            uint32_t duration = micros() - startTime;
-            if (duration < 100)
-            {
-               // do not set this in the table
-               oldState = newState;
-               startTime = micros();
-               continue;
-            }
-            else
-            {
-               table[index] = duration;
-               index++;
-               oldState = newState;
-               startTime = micros();
-
-//               Serial.println(F("Mam cos"));
-//               Serial.println(duration);
-//               Serial.println(currentMicros - firstChangeTimeStamp);
-//
-//               return true;
-            }
-
-            currentState = MY_STATE_COLLECT_DATA;
-            break;
-         }
-         case MY_STATE_COLLECT_DATA:
-         {
-            while (oldState == (newState = digitalRead(RECEIVER_PIN)))
-            {
-               currentMicros = micros();
-               if (oldState == HIGH)
-               {
-                  if ((currentMicros - startTime) > 3000000)
-                  {
-                     currentState = MY_STATE_FINISHED;
-                     break;
-                  }
-               }
-            }
-
-            currentMicros = micros();
-            table[index] = currentMicros - startTime;
-            oldState = newState;
-            startTime = currentMicros;
-            if (++index >= BUFFER_SIZE)
-            {
-               currentState = MY_STATE_JUST_COUNT;
-            }
-            break;
-         }
-         case MY_STATE_JUST_COUNT:
-         {
-            while (oldState == (newState = digitalRead(RECEIVER_PIN)))
-            {
-               currentMicros = micros();
-               if (oldState == HIGH)
-               {
-                  if ((currentMicros - startTime) > 3000000)
-                  {
-                     currentState = MY_STATE_FINISHED;
-                     break;
-                  }
-               }
-            }
-
-            currentMicros = micros();
-            oldState = newState;
-            index++;
-            startTime = currentMicros;
-            break;
-         }
-      }
-   }
-
-   return true;
-}
 
 #define MY_STATE2_IDLE 0
 #define MY_STATE2_WAITING_FOR_USER_CLICK 1
