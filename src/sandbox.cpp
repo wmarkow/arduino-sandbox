580aad2c3ed678792673ec38b4d831ae89c9bb9d
Witold Markowski <witold.a.markowski@gmail.com>
2018-10-02 20:27:04 +0100

    Additional delay at startup.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -11,6 +11,11 @@ void setup()
    // initialize digital pin LED_BUILTIN as an output.
    pinMode(LED_BUILTIN, OUTPUT);
    Serial.begin(115200);
+
+   // Wait for VS1053 to power up
+   // otherwise the system might not start up correctly
+   delay(3000);
+
    SPI.begin();
    webRadioClient.begin(ssid, password);
 }
