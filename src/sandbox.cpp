169f127665788d785f86083d9baafcc4b33e30c0
Witold Markowski <witold.a.markowski@gmail.com>
2019-03-06 20:33:21 +0000

    Add code for a basic telegraph-key.

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -1,39 +1,74 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
-*/
-
 #include "Arduino.h"
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+#define TELEGRAPH_KEY_PIN 8
+#define BUZZER_PIN 3
+#define KEY_DEBOUNCE_MILLIS 10
+
+unsigned long lastKeyCheckTime = 0;
+int lastKeyState = HIGH;
+int lastKeyDebounceState = HIGH;
+
+void setup()
+{
+   Serial.begin(9600);
+
+   pinMode(TELEGRAPH_KEY_PIN, INPUT_PULLUP);
+   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(BUZZER_PIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void loop()
+{
+   // check button
+   if (millis() + KEY_DEBOUNCE_MILLIS >= lastKeyCheckTime)
+   {
+      lastKeyCheckTime = millis();
+
+      int currentKeyState = digitalRead(TELEGRAPH_KEY_PIN);
+      if (currentKeyState == lastKeyDebounceState)
+      {
+         // we have a stable debounced key state
+         if (lastKeyState == currentKeyState)
+         {
+            // still the same, nothing has changed
+         }
+         else
+         {
+            // key state has changed
+            lastKeyState = currentKeyState;
+
+            if (currentKeyState == HIGH)
+            {
+               // PRESSED -> RELEASED
+               Serial.println(F("klucz zwolnioniy"));
+
+               digitalWrite(LED_BUILTIN, LOW);
+
+               analogWrite(BUZZER_PIN, 0);
+            }
+            else
+            {
+               // RELEASED -> PRESSED
+               Serial.println(F("klucz nacisniety"));
+
+               digitalWrite(LED_BUILTIN, HIGH);
+               analogWrite(BUZZER_PIN, 128);
+            }
+         }
+      }
+      else
+      {
+         lastKeyDebounceState = currentKeyState;
+      }
+   }
+
+//   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+//   delay(1000);                       // wait for a second
+//   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+//   delay(1000);                       // wait for a second
+}
+
+bool isButtonPressed()
+{
+
 }
