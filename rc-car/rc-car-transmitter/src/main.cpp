c8e3164622e9ed725519588cb6e965dc6b66368c
Witold Markowski <witold.a.markowski@gmail.com>
2019-07-31 22:30:14 +0100

    Read joystick state. Read one button state.

--- rc-car/rc-car-transmitter/src/main.cpp
+++ rc-car/rc-car-transmitter/src/main.cpp
@@ -1,12 +1,34 @@
 #include <Arduino.h>
 
+#define JOYSTICK_X_INPUT A0
+#define JOYSTICK_Y_INPUT A1
+#define TURBO_BUTTON_INPUT 2
+
+
+unsigned long lastDisplayTime;
+
 void setup()
 {
-   Serial.begin(115200);
+    pinMode(TURBO_BUTTON_INPUT, INPUT);
 
+    Serial.begin(9600);
+
+    lastDisplayTime = 0;
 }
 
 void loop(void)
 {
+    int16_t joystickX = analogRead(JOYSTICK_X_INPUT);
+    int16_t joystickY = analogRead(JOYSTICK_Y_INPUT);
+    uint8_t turboButton = digitalRead(TURBO_BUTTON_INPUT);
 
+    if (millis() - lastDisplayTime > 100)
+    {
+        char tbs[50];
+        sprintf(tbs, "X=%04d Y=%04d T=%1d", joystickX, joystickY, turboButton);
+
+        Serial.println(tbs);
+
+        lastDisplayTime = millis();
+    }
 }
