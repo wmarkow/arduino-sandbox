e27f8c6ce22571efc64827a94dac536c81c9377d
Witold Markowski <witold.a.markowski@gmail.com>
2019-08-02 22:01:34 +0100

    Send TCP through LocalMeshNode.

--- rc-car/rc-car-transmitter/src/transmitter.cpp
+++ rc-car/rc-car-transmitter/src/transmitter.cpp
@@ -85,7 +85,7 @@ void loop(void)
         int8_t speedInPercent = map(joystickY, -512, 512, -100, 100);
         rcDatagrm.speedInPercent = speedInPercent;
         // TODO: send this as UDP
-        if (radioRF24.sendTcp(DST_ADDRESS, (uint8_t*) &rcDatagrm,
+        if (LocalMeshNode.sendTcp(DST_ADDRESS, (uint8_t*) &rcDatagrm,
                 sizeof(RCDatagram)))
         {
             // Serial.println(F("sent"));
