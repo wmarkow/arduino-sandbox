6c07c47cbbb9cb106fd316ddd1e7fb6253792b0d
Witold Markowski <witold.a.markowski@gmail.com>
2020-11-29 11:11:28 +0000

    [Trains] Add NimBLE-Arduino minimal example which works.

---
+++ NimBLE-Arduino-min-example/NimBLE_Server.ino
@@ -0,0 +1,122 @@
+
+/** NimBLE_Server Demo:
+ *
+ *  Demonstrates many of the available features of the NimBLE server library.
+ *  
+ *  Created: on March 22 2020
+ *      Author: H2zero
+ * 
+*/
+
+#include <NimBLEDevice.h>
+
+static NimBLEServer* pServer;
+
+/**  None of these are required as they will be handled by the library with defaults. **
+ **                       Remove as you see fit for your needs                        */  
+class ServerCallbacks: public NimBLEServerCallbacks {
+    void onConnect(NimBLEServer* pServer) {
+        Serial.println("Client connected");
+        Serial.println("Multi-connect support: start advertising");
+        NimBLEDevice::startAdvertising();
+    };
+    /** Alternative onConnect() method to extract details of the connection. 
+     *  See: src/ble_gap.h for the details of the ble_gap_conn_desc struct.
+     */  
+    void onConnect(NimBLEServer* pServer, ble_gap_conn_desc* desc) {
+        Serial.print("Client address: ");
+        Serial.println(NimBLEAddress(desc->peer_ota_addr).toString().c_str());
+        /** We can use the connection handle here to ask for different connection parameters.
+         *  Args: connection handle, min connection interval, max connection interval
+         *  latency, supervision timeout.
+         *  Units; Min/Max Intervals: 1.25 millisecond increments.
+         *  Latency: number of intervals allowed to skip.
+         *  Timeout: 10 millisecond increments, try for 5x interval time for best results.  
+         */
+        pServer->updateConnParams(desc->conn_handle, 24, 48, 0, 60);
+    };
+    void onDisconnect(NimBLEServer* pServer) {
+        Serial.println("Client disconnected - start advertising");
+        NimBLEDevice::startAdvertising();
+    };
+    
+/********************* Security handled here **********************
+****** Note: these are the same return values as defaults ********/
+    uint32_t onPassKeyRequest(){
+        Serial.println("Server Passkey Request");
+        /** This should return a random 6 digit number for security 
+         *  or make your own static passkey as done here.
+         */
+        return 123456; 
+    };
+
+    bool onConfirmPIN(uint32_t pass_key){
+        Serial.print("The passkey YES/NO number: ");Serial.println(pass_key);
+        /** Return false if passkeys don't match. */
+        return true; 
+    };
+
+    void onAuthenticationComplete(ble_gap_conn_desc* desc){
+        /** Check that encryption was successful, if not we disconnect the client */  
+        if(!desc->sec_state.encrypted) {
+            NimBLEDevice::getServer()->disconnect(desc->conn_handle);
+            Serial.println("Encrypt connection failed - disconnecting client");
+            return;
+        }
+        Serial.println("Starting BLE work!");
+    };
+};
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting NimBLE Server");
+
+    /** sets device name */
+    NimBLEDevice::init("NimBLE-Arduino");
+
+    /** Optional: set the transmit power, default is 3db */
+    NimBLEDevice::setPower(ESP_PWR_LVL_P9); /** +9db */
+    
+    /** Set the IO capabilities of the device, each option will trigger a different pairing method.
+     *  BLE_HS_IO_DISPLAY_ONLY    - Passkey pairing
+     *  BLE_HS_IO_DISPLAY_YESNO   - Numeric comparison pairing
+     *  BLE_HS_IO_NO_INPUT_OUTPUT - DEFAULT setting - just works pairing
+     */
+
+    /** 2 different ways to set security - both calls achieve the same result.
+     *  no bonding, no man in the middle protection, secure connections.
+     *   
+     *  These are the default values, only shown here for demonstration.   
+     */ 
+    NimBLEDevice::setSecurityAuth(/*BLE_SM_PAIR_AUTHREQ_BOND | BLE_SM_PAIR_AUTHREQ_MITM |*/ BLE_SM_PAIR_AUTHREQ_SC);
+
+    pServer = NimBLEDevice::createServer();
+    pServer->setCallbacks(new ServerCallbacks());
+
+
+    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+
+    /** If your device is battery powered you may consider setting scan response
+     *  to false as it will extend battery life at the expense of less data sent.
+     */
+    pAdvertising->setScanResponse(true);
+    pAdvertising->start();
+
+    Serial.println("Advertising Started");
+}
+
+
+void loop() {
+  /** Do your thing here, this just spams notifications to all connected clients */
+    if(pServer->getConnectedCount()) {
+        NimBLEService* pSvc = pServer->getServiceByUUID("BAAD");
+        if(pSvc) {
+            NimBLECharacteristic* pChr = pSvc->getCharacteristic("F00D");
+            if(pChr) {
+                pChr->notify(true);
+            }
+        }
+    }
+    
+  delay(2000);
+}
