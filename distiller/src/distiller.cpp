e6ef03967bc6626022269178076723b12ed11abd
Witold Markowski <witold.a.markowski@gmail.com>
2021-02-07 12:50:56 +0000

    [Distiller] Add initial version for distiller project.

---
+++ distiller/src/distiller.cpp
@@ -0,0 +1,19 @@
+/*
+ * distiller.cpp
+ *
+ *  Created on: 7 lut 2021
+ *      Author: wmarkowski
+ */
+
+
+#include <Arduino.h>
+
+void setup()
+{
+   Serial.begin(115200);
+}
+
+void loop()
+{
+
+}
