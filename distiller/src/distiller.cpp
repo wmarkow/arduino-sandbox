3e0c8629ec375ea1c2ffeab795d3b07d8e9cc3da
Witold Markowski <witold.a.markowski@gmail.com>
2021-04-21 18:00:39 +0100

    [Distiller] Move to ESP32 DEVKIT V1 DOIT.

--- distiller/src/distiller.cpp
+++ distiller/src/distiller.cpp
@@ -11,10 +11,10 @@
 #include "drivers/DS18B20.h"
 
 #define WATER_FLOW_SENSOR_PIN 2
-#define TERMOMETER_PIN 3
+#define TERMOMETER_COLD_WATER_PIN 22
 
 WaterFlowSensor waterFlowSensor(WATER_FLOW_SENSOR_PIN);
-DS18B20 thermometer(TERMOMETER_PIN);
+DS18B20 thermometer(TERMOMETER_COLD_WATER_PIN);
 
 unsigned long lastCheckTime;
 
@@ -22,49 +22,49 @@ void water_sensor_event();
 
 void setup()
 {
-   Serial.begin(115200);
+    Serial.begin(115200);
 
-   waterFlowSensor.init();
+    waterFlowSensor.init();
 
-   attachInterrupt(digitalPinToInterrupt(WATER_FLOW_SENSOR_PIN),
-         water_sensor_event, RISING);
+    attachInterrupt(digitalPinToInterrupt(WATER_FLOW_SENSOR_PIN),
+            water_sensor_event, RISING);
 
-   lastCheckTime = millis();
+    lastCheckTime = millis();
 
-   thermometer.begin();
+    thermometer.begin();
 }
 
 void loop()
 {
-   waterFlowSensor.loop();
+    waterFlowSensor.loop();
 
-   unsigned long measureSpanMillis = millis() - lastCheckTime;
+    unsigned long measureSpanMillis = millis() - lastCheckTime;
 
-   if (measureSpanMillis >= 1000L)
-   {
-      // display water flow
-      Serial.print(waterFlowSensor.getRpm());
-      Serial.print(" ; ");
+    if (measureSpanMillis >= 1000L)
+    {
+        // display water flow
+        Serial.print(waterFlowSensor.getRpm());
+        Serial.print(" ; ");
 
-      // display temperature
-      float temp;
-      if (thermometer.readTempC(&temp) == DS18B20_RESULT_OK)
-      {
-         Serial.print(temp);
-      }
-      else
-      {
-         Serial.print("ERR");
-      }
-      Serial.print(" ; ");
+        // display temperature
+        float temp;
+        if (thermometer.readTempC(&temp) == DS18B20_RESULT_OK)
+        {
+            Serial.print(temp);
+        }
+        else
+        {
+            Serial.print("ERR");
+        }
+        Serial.print(" ; ");
 
-      // display time span
-      Serial.println(measureSpanMillis);
-      lastCheckTime = millis();
-   }
+        // display time span
+        Serial.println(measureSpanMillis);
+        lastCheckTime = millis();
+    }
 }
 
 void water_sensor_event()
 {
-   waterFlowSensor.tick();
+    waterFlowSensor.tick();
 }
