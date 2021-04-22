15cac62537d596ae8f1d6c0029d75347daba581c
Witold Markowski <witold.a.markowski@gmail.com>
2021-04-22 15:38:36 +0100

    [Distiller] Improvement in DS18B20 hardware failures.

--- distiller/src/drivers/DS18B20.cpp
+++ distiller/src/drivers/DS18B20.cpp
@@ -4,7 +4,7 @@
 DS18B20::DS18B20(uint8_t pin) :
         oneWireBus(pin), thermometerBus(&oneWireBus)
 {
-
+    this->reinit = false;
 }
 
 void DS18B20::begin()
@@ -22,12 +22,21 @@ void DS18B20::begin()
     {
         RF24LOGGER_info(vendorId, "DS18B20 thermometer NOT found!");
     }
+
+    reinit = false;
 }
 
 uint8_t DS18B20::readTempC(float *value)
 {
+    if(reinit)
+    {
+        begin();
+    }
+
     if (thermometerBus.getDeviceCount() == 0)
     {
+        reinit = true;
+
         return DS18B20_RESULT_ERR;
     }
 
@@ -36,6 +45,8 @@ uint8_t DS18B20::readTempC(float *value)
         // Parasite mode (when Vdd is connected to the ground) is considered by the project to be not supported.
         // It has been noticed that parasite mode is mostly also detected (not in 100% cases) when Vdd pin is left
         // unconnected; this helps to improve the hardware failure handling.
+        reinit = true;
+
         return DS18B20_RESULT_ERR;
     }
 
@@ -44,6 +55,8 @@ uint8_t DS18B20::readTempC(float *value)
 
     if (temp == DEVICE_DISCONNECTED_C)
     {
+        reinit = true;
+
         return DS18B20_RESULT_ERR;
     }
 
--- distiller/src/drivers/DS18B20.h
+++ distiller/src/drivers/DS18B20.h
@@ -17,6 +17,7 @@ public:
 private:
    OneWire oneWireBus;
    DallasTemperature thermometerBus;
+   bool reinit;
 };
 
 
