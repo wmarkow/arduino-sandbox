41870e943421ede7373d9af3e46bcd631654b7bc
Witold Markowski <witold.a.markowski@gmail.com>
2021-10-30 22:35:48 +0100

    [Welder] Preparation to use potentiometer to control wire speed.

--- inverter-welder/examples/SpoolGun/SpoolGun.ino
+++ inverter-welder/examples/SpoolGun/SpoolGun.ino
@@ -5,6 +5,12 @@
 #define MOTOR_STEPS 200
 #define RPM 60
 
+// Define min and max possible wire speeds in [m/min]
+#define MIN_WIRE_SPEED 0.5
+#define MAX_WIRE_SPEED 2.0
+// Define wire feeder gear diameter in m
+#define WIRE_FEEDER_GEAR_DIAMETER 0.011
+
 // Since microstepping is set externally, make sure this matches the selected mode
 // If it doesn't, the motor will move at a different RPM than chosen
 // 1=full step, 2=half step etc.
@@ -17,6 +23,8 @@
 
 // Start/stop button pin
 #define PIN_BUTTON 9 // from X limit switch of CNC shield
+// Wire speed potentiometer pin
+#define PIN_WIRE_SPEED A0 // from Abort pin of CNC shield
 
 BasicStepperDriver stepper(MOTOR_STEPS, PIN_DIR, PIN_STEP, PIN_ENABLE);
 
@@ -31,6 +39,8 @@ void setup()
 
    pinMode(PIN_BUTTON, INPUT);
    digitalWrite(PIN_BUTTON, HIGH);
+
+   pinMode(PIN_WIRE_SPEED, INPUT);
 
    stepper.setEnableActiveState(LOW);
    stepper.begin(RPM, MICROSTEPS);
@@ -71,13 +81,20 @@ void loopForContinuousWelding()
 {
    if (lastButtonState == LOW)
    {
-      // switch motor on
-      // make one rotation in one second
-      int stepsPerSecond = RPM / 60 * MOTOR_STEPS;
-      double shotDurationSecond = 1;
-      int steps = stepsPerSecond * shotDurationSecond;
+      // check wire speed potentiometer
+      float analogVal = analogRead(PIN_WIRE_SPEED);
+      analogVal = 0;
+      float wireSpeed = (analogVal / 1024.0) * (MAX_WIRE_SPEED - MIN_WIRE_SPEED) + MIN_WIRE_SPEED;
+      float rpm = wireSpeed / (PI * WIRE_FEEDER_GEAR_DIAMETER);
+
+      Serial.print(wireSpeed);
+      Serial.print(" ");
+      Serial.println(rpm);
+      // Switch motor on. Calculate move to fill one second.
+      int stepsPerSecond = rpm / 60 * MOTOR_STEPS;
+      stepper.setRPM(rpm);
       stepper.enable();
-      stepper.startMove(steps);
+      stepper.startMove(stepsPerSecond);
 
       // when button is pressed execute stepper action
       stepper.nextAction();
