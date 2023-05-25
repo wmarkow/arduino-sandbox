4ce6b6d1dc8bedeafd3d37ed3ef13c69aebd2bc0
Witold Markowski <witold.a.markowski@gmail.com>
2023-05-25 20:13:07 +0100

    [FoxHunting] Sending and receiving MOE morse signal.

--- fox-hunting/examples/Receiver/Receiver.cpp
+++ fox-hunting/examples/Receiver/Receiver.cpp
@@ -5,6 +5,8 @@ HC12 hc12;
 const uint8_t BUZZER_PIN = 3;
 uint32_t lastBeepMillis = 0;
 
+uint8_t buzzerState = 0; 
+
 void setup()
 {
     Serial.begin(115200);
@@ -17,6 +19,7 @@ void setup()
 void loop()
 {
     uint8_t byte = 0;
+    uint8_t newBuzzerState = buzzerState;
     while(hc12.available() > 0)
     {
         // read one byte
@@ -25,16 +28,28 @@ void loop()
             if(byte == 255)
             {
                 // read what has been sent
-                Serial.println("Received byte");
-                tone(BUZZER_PIN, 1000);
+                Serial.print(millis());
+                Serial.println(" REBY");
+                newBuzzerState = 1;
                 lastBeepMillis = millis();
             }
         }
+
+        if(buzzerState == 0 && newBuzzerState == 1)
+        {
+            Serial.print(millis());
+            Serial.println(" BUZZ");
+            buzzerState = 1;
+            tone(BUZZER_PIN, 1000);
+        }
     }
 
-    if(lastBeepMillis + 200 < millis())
+    if((lastBeepMillis + 100 < millis()) && buzzerState == 1)
     {
+        Serial.print(millis());
+        Serial.println(" OFF");
         noTone(BUZZER_PIN);
+        buzzerState = 0;
     }
 
 }
--- fox-hunting/examples/Transmitter/Transmitter.cpp
+++ fox-hunting/examples/Transmitter/Transmitter.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include <HC12.h>
 
+const uint16_t DOT_DURATION_MILLIS = 200;
+
 HC12 hc12;
+
+void sendDot();
+void sendDash();
 
 void setup()
 {
@@ -12,11 +17,53 @@ void setup()
 
 void loop()
 {
-    for(uint8_t q = 0 ; q < 5 ; q ++)
+    // send M
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(3 * DOT_DURATION_MILLIS);
+    
+    // send O
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(3 * DOT_DURATION_MILLIS);
+
+    // send E
+    sendDot();
+    delay(7 * DOT_DURATION_MILLIS);
+   
+    delay(1000);
+}
+
+void sendDot()
+{
+    Serial.print(millis());
+    Serial.println(" DOT begin");
+    uint32_t startTime = millis();
+    while(millis() < startTime + DOT_DURATION_MILLIS)
     {
         hc12.write(255);
-        delay(100);
+        delay(25);
     }
-   
-    delay(500);
+
+    Serial.print(millis());
+    Serial.println(" DOT end");
+}
+
+void sendDash()
+{
+    Serial.print(millis());
+    Serial.println(" DASH begin");
+    uint32_t startTime = millis();
+    while(millis() < startTime + 3 * DOT_DURATION_MILLIS)
+    {
+        hc12.write(255);
+        delay(25);
+    }
+
+    Serial.print(millis());
+    Serial.println(" DASH end");
 }
