fd6a7c85788e6bcf9d51e9886772965cebbf959b
Witold Markowski <witold.a.markowski@gmail.com>
2023-06-24 07:59:42 +0100

    [FoxHunting] Beep when receiving data.

--- fox-hunting/examples/FS1000AReceiver/FS1000AReceiver.cpp
+++ fox-hunting/examples/FS1000AReceiver/FS1000AReceiver.cpp
@@ -3,6 +3,10 @@
 
 RH_ASK driver;
 
+const uint8_t BUZZER_PIN = 3;
+uint32_t lastBeepMillis = 0;
+uint8_t buzzerState = 0;
+
 void setup()
 {
     // Receiver pin must be connected to Arduino pin D11
@@ -19,14 +23,34 @@ void setup()
 
 void loop()
 {
+    uint8_t newBuzzerState = buzzerState;
+
     uint8_t buf[12];
     uint8_t buflen = sizeof(buf);
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
-      int i;
-      // Message with a good checksum received, dump it.
-      Serial.print(millis());
-      Serial.print(" Message: ");
-      Serial.println((char*)buf);         
+        // Message with a good checksum received, dump it.
+        Serial.print(millis());
+        Serial.print(" Message: ");
+        Serial.println((char*)buf);   
+
+        newBuzzerState = 1;
+        lastBeepMillis = millis();  
+
+        if(buzzerState == 0 && newBuzzerState == 1)
+        {
+            Serial.print(millis());
+            Serial.println(" BUZZ");
+            buzzerState = 1;
+            analogWrite(BUZZER_PIN, 127);
+        }    
+    }
+
+    if((lastBeepMillis + 60 < millis()) && buzzerState == 1)
+    {
+        Serial.print(millis());
+        Serial.println(" OFF");
+        analogWrite(BUZZER_PIN, 0);
+        buzzerState = 0;
     }
 }
