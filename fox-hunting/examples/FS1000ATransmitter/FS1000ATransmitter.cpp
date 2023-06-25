13bc9a20bdf0f928adb4aec4a52dff5ab57951e5
Witold Markowski <witold.a.markowski@gmail.com>
2023-06-25 09:41:21 +0100

    [FoxHunting] Sending MOE Morse Code signal.

--- fox-hunting/examples/FS1000ATransmitter/FS1000ATransmitter.cpp
+++ fox-hunting/examples/FS1000ATransmitter/FS1000ATransmitter.cpp
@@ -1,7 +1,13 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
 
+const uint16_t DOT_DURATION_MILLIS = 150;
+const uint16_t DASH_DURATION_MILLIS = 3 * DOT_DURATION_MILLIS;
+
 RH_ASK driver;
+
+void sendDot();
+void sendDash();
 
 void setup()
 {
@@ -19,10 +25,55 @@ void setup()
 
 void loop()
 {
-    Serial.print("sending packet... ");
-    const char *msg = "Hello World!";
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    Serial.println("done.");
+    // send M
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(3 * DOT_DURATION_MILLIS);
+    
+    // send O
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(DOT_DURATION_MILLIS);  
+    sendDash();
+    delay(3 * DOT_DURATION_MILLIS);
+
+    // send E
+    sendDot();
+    delay(7 * DOT_DURATION_MILLIS);
+   
     delay(1000);
+}
+
+void sendDot()
+{
+    Serial.print(millis());
+    Serial.println(" DOT begin");
+    uint32_t startTime = millis();
+    while(millis() < startTime + DOT_DURATION_MILLIS)
+    {
+        const char *msg = "Hello World!";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+    }
+
+    Serial.print(millis());
+    Serial.println(" DOT end");
+}
+
+void sendDash()
+{
+    Serial.print(millis());
+    Serial.println(" DASH begin");
+    uint32_t startTime = millis();
+    while(millis() < startTime + DASH_DURATION_MILLIS)
+    {
+        const char *msg = "Hello World!";
+        driver.send((uint8_t *)msg, strlen(msg));
+        driver.waitPacketSent();
+    }
+
+    Serial.print(millis());
+    Serial.println(" DASH end");
 }
