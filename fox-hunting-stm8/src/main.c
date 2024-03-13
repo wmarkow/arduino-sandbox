189138bf6c7c3d9b551f0112112b5f3805b3ad8c
Witold Markowski <witold.a.markowski@gmail.com>
2024-03-13 12:35:28 +0100

    [FoxHunting] Make 2FSK example a default one (hearable on PMR with FM demodulation).

--- fox-hunting-stm8/src/main.c
+++ fox-hunting-stm8/src/main.c
@@ -1,3 +1,3 @@
-//#include "../examples/FakeF3ETransmitter/FakeF3ETransmitter.c"
 //#include "../examples/CWTransmitter/CWTransmitter.c"
-#include "../examples/OOKTransmitter/OOKTransmitter.c"
+//#include "../examples/OOKTransmitter/OOKTransmitter.c"
+#include "../examples/FakeF3ETransmitter/FakeF3ETransmitter.c"
