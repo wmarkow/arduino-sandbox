a511dfc37738d8bd5866a8ab8173346c6e446d85
Witold Markowski <witold.a.markowski@gmail.com>
2024-07-24 21:17:04 +0100

    [FoxHunting] Move morse related code to its own files.

--- fox-hunting-stm8/examples/FoxTransmitter/FoxTransmitter.c
+++ fox-hunting-stm8/examples/FoxTransmitter/FoxTransmitter.c
@@ -8,23 +8,12 @@
 
 uint8_t foxState;
 
-#define DOT_DURATION_MILLIS  150
-#define DASH_DURATION_MILLIS 3 * DOT_DURATION_MILLIS
-
 void stm8s_sleep(uint8_t tbr, uint8_t apr);
 #define STM8_S_SLEEP_250_MILLISEC() stm8s_sleep(10, 62)
 #define STM8_S_SLEEP_500_MILLISEC() stm8s_sleep(11, 62)
 #define STM8_S_SLEEP_2_25_SEC() stm8s_sleep(14, 28)
 #define STM8_S_SLEEP_5_SEC() stm8s_sleep(14, 62)
 #define STM8_S_SLEEP_20_SEC() stm8s_sleep(15, 41)
-
-void sendDot();
-void sendDash();
-void sendMOE();
-void sendMOI();
-void sendMOS();
-void sendMOH();
-void sendMO5();
 
 void setup()
 {
@@ -165,151 +154,6 @@ void loop()
 
         return;
     }
-}
-
-void sendMOE()
-{
-    Serial_println_s("sending MOE...");
-
-    // send M
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-    
-    // send O
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-
-    // send E
-    sendDot();
-    delay(7 * DOT_DURATION_MILLIS);
-}
-
-void sendMOI()
-{
-    Serial_println_s("sending MOI...");
-
-    // send M
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-    
-    // send O
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-
-    // send I
-    sendDot();
-    delay(DOT_DURATION_MILLIS);  
-    sendDot();
-    delay(7 * DOT_DURATION_MILLIS);
-}
-
-void sendMOS()
-{
-    Serial_println_s("sending MOS...");
-
-    // send M
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-    
-    // send O
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-
-    // send S
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS);  
-    sendDot();
-    delay(7 * DOT_DURATION_MILLIS);
-}
-
-void sendMOH()
-{
-    Serial_println_s("sending MOH...");
-
-    // send M
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-    
-    // send O
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-
-    // send H
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS);  
-    sendDot();
-    delay(7 * DOT_DURATION_MILLIS);
-}
-
-void sendMO5()
-{
-    Serial_println_s("sending MO5...");
-
-    // send M
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-    
-    // send O
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(DOT_DURATION_MILLIS);  
-    sendDash();
-    delay(3 * DOT_DURATION_MILLIS);
-
-    // send 5
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS); 
-    sendDot();
-    delay(DOT_DURATION_MILLIS);  
-    sendDot();
-    delay(7 * DOT_DURATION_MILLIS);
-}
-
-void inline sendDot()
-{
-    fake_f3e_tone(700, ((unsigned long)DOT_DURATION_MILLIS) * ((unsigned long)1000));
-}
-
-void inline sendDash()
-{
-    fake_f3e_tone(700, ((unsigned long)DASH_DURATION_MILLIS) * ((unsigned long)1000));
 }
 
 void stm8s_sleep(uint8_t tbr, uint8_t apr)
---
+++ fox-hunting-stm8/lib/core/services/morse.h
@@ -0,0 +1,8 @@
+
+void sendDot();
+void sendDash();
+void sendMOE();
+void sendMOI();
+void sendMOS();
+void sendMOH();
+void sendMO5();
