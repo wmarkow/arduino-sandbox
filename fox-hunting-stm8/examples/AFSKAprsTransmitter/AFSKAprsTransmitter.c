88b17ffe11fd58772ad822f4d3279d3407d4c689
Witold Markowski <witold.a.markowski@gmail.com>
2024-12-02 19:25:53 +0000

    [FoxHunting] Put more comments and make the configuration easier.

--- fox-hunting-stm8/examples/AFSKAprsTransmitter/AFSKAprsTransmitter.c
+++ fox-hunting-stm8/examples/AFSKAprsTransmitter/AFSKAprsTransmitter.c
@@ -64,15 +64,15 @@ void loop()
     // Enter the Tx state on channel 0
     fsk_start_tx(0);
 
-    char minimalFrame[] = {'S', 'Q', '3', 'E', 'T', ' ', ' ', 'S', 'P', '3', 'W', 'A', 'M', ' ', 'W', 'I', 'D', 'E', '2', ' ', ' ', 0x03, 0xF0, ':', 'B', 'L', 'N', '0', ' ', ' ', ' ', ' ', ' ', ':', 'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ', 'H', 'C', '1', '2', '#', '#'};
+    char minimalFrame[] = {'S', 'Q', '3', 'E', 'T', ' ', '0', 'S', 'P', '3', 'W', 'A', 'M', '0', 'W', 'I', 'D', 'E', '2', ' ', '2', 0x03, 0xF0, ':', 'B', 'L', 'N', '0', ' ', ' ', ' ', ' ', ' ', ':', 'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ', 'H', 'C', '1', '2', '#', '#'};
     uint8_t frameLength = sizeof(minimalFrame);
 
     // The SSID in the Destination Address field of all packets is coded to specify
     // the APRS digipeater path. See APRS101.pdf page 15.
-    minimalFrame[6] = 0b00110000; // C=0, RR=11, SSID=0, it uses digipiter "VIA"
-    minimalFrame[13] = 0b00110000; // C=0, RR=11, SSID=0
-    // define "VIA" as 'W', 'I', 'D', 'E', '2', ' ' with a correct SSID of 2 (means to use WIDE2-2)
-    minimalFrame[20] = 0b00110010; // C=0, RR=11, SSID=2
+    // minimalFrame[6] = 0b00110000; // C=0, RR=11, SSID=0, it uses digipiter "VIA". That's the same as using ASCII of character '0'.
+    // minimalFrame[13] = 0b00110000; // C=0, RR=11, SSID=0. That's the same as using ASCII of character '0'.
+    // define "VIA" as 'W', 'I', 'D', 'E', '2', ' ' with a correct SSID of 2 (means to use WIDE2-2). 
+    // minimalFrame[20] = 0b00110010; // C=0, RR=11, SSID=2. That's the same as using ASCII of character '2'.
     
     // shift address bytes one bit to the left
     for(uint8_t q = 0 ; q < 21 ; q ++)
